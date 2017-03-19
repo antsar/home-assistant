760f57a72b78dfe30b6cd86fcbc98c9cee7809af
Anton Sarukhanov <code@ant.sr>
2017-03-19 12:06:48 -0400

    Debugging HA/Avion.

--- homeassistant/components/light/avion.py
+++ homeassistant/components/light/avion.py
@@ -9,13 +9,13 @@ import time
 
 import voluptuous as vol
 
-from homeassistant.const import CONF_API_KEY, CONF_DEVICES, CONF_NAME, CONF_MAC
+from homeassistant.const import CONF_API_KEY, CONF_DEVICES, CONF_NAME, CONF_ID
 from homeassistant.components.light import (
     ATTR_BRIGHTNESS, SUPPORT_BRIGHTNESS, Light,
     PLATFORM_SCHEMA)
 import homeassistant.helpers.config_validation as cv
 
-REQUIREMENTS = ['https://github.com/antsar/python-avion/archive/hotfix.zip#avion==hotfix']
+REQUIREMENTS = ['https://github.com/antsar/python-avion/archive/hotfix-hadebug.zip#avion==hotfix-hadebug']
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -23,27 +23,29 @@ SUPPORT_AVION_LED = (SUPPORT_BRIGHTNESS)
 
 DEVICE_SCHEMA = vol.Schema({
     vol.Optional(CONF_NAME, default=''): cv.string,
+    vol.Required(CONF_API_KEY): cv.string,
+    vol.Optional(CONF_ID): cv.positive_int,
 })
 
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_DEVICES, default={}): {cv.string: DEVICE_SCHEMA},
-    vol.Required(CONF_MAC): cv.string,
-    vol.Required(CONF_API_KEY): cv.string,
 })
 
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
     """Set up an Avion switch."""
     lights = []
-    for device_id, device_config in config[CONF_DEVICES].items():
+    connected_lights = {}
+    for address, device_config in config[CONF_DEVICES].items():
         device = {}
-        device['name'] = device_config[CONF_NAME] or name
-        device['id'] = device_id
-        device['key'] = config[CONF_API_KEY]
-        device['address'] = config[CONF_MAC]
-        light = AvionLight(device)
+        device['name'] = device_config[CONF_NAME]
+        device['id'] = device_config[CONF_ID]
+        device['key'] = device_config[CONF_API_KEY]
+        device['address'] = address
+        light = AvionLight(device, connected_lights)
         if light.is_valid:
             lights.append(light)
+            connected_lights[(device['address'], device['key'])] = light
 
     add_devices(lights)
 
@@ -51,7 +53,7 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
 class AvionLight(Light):
     """Representation of an Avion light."""
 
-    def __init__(self, device):
+    def __init__(self, device, connected_lights=[]):
         """Initialize the light."""
         # pylint: disable=import-error
         import avion
@@ -63,22 +65,26 @@ class AvionLight(Light):
         self._id = device['id']
         self._brightness = 255
         self._state = False
-        self._switch = avion.avion(self._address, self._key)
-        initial = time.time()
-        while True:
-            if time.time() - initial >= 10:
-                raise Exception('Failed to connect.')
-            try:
-                self._switch.connect()
-                break
-            except btle.BTLEException:
-                pass
+        if ((self._address, self._key) in connected_lights):
+            self._switch = connected_lights[(self._address, self._key)]
+        else:
+            self._switch = avion.avion(self._address, self._key)
+            initial = time.time()
+            while True:
+                if time.time() - initial >= 5:
+                    raise Exception('Avion failed to connect to {}.'
+                                    .format(self._address))
+                try:
+                    self._switch.connect()
+                    break
+                except btle.BTLEException:
+                    time.sleep(0.5)
         self.is_valid = True
 
     @property
     def unique_id(self):
         """Return the ID of this light."""
-        return "{}.{}".format(self.__class__, self._address)
+        return "{}.{}.{}".format(self.__class__, self._address, self._id)
 
     @property
     def name(self):
@@ -112,7 +118,7 @@ class AvionLight(Light):
 
     def set_state(self, brightness):
         """Set the state of this lamp to the provided brightness."""
-        self._switch.set_brightness(brightness)
+        self._switch.set_brightness(brightness, self._id)
         return True
 
     def turn_on(self, **kwargs):
@@ -122,10 +128,10 @@ class AvionLight(Light):
         if brightness is not None:
             self._brightness = brightness
 
-        self.set_state(self.brightness, self._id)
+        self.set_state(self.brightness)
         self._state = True
 
     def turn_off(self, **kwargs):
         """Turn the specified or all lights off."""
-        self.set_state(0, self._id)
+        self.set_state(0)
         self._state = False
